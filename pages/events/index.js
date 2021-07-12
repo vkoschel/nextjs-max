11f1edffd028d9908cf3c4e7c9349b91380fc11f
vkoschel <victorkoschel@gmail.com>
2021-07-12 11:21:39 -0300

    search component

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,8 +1,15 @@
+import EventList from "../../components/events/event-list";
+import { getAllEvents } from "../../dummy-data";
+import EventsSearch from "../../components/events/events-search";
+import { Fragment } from "react";
+
 function AllEventsPage() {
+  const events = getAllEvents();
   return (
-    <div>
-      <h1>All Events</h1>
-    </div>
+    <Fragment>
+      <EventsSearch />
+      <EventList items={events} />
+    </Fragment>
   );
 }
 
